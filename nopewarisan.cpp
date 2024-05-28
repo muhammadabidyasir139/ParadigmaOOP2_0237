0f7c615332962f848fc6a5b85cc8d11380b46dbf
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-05-28 15:29:59 +0700

    membuat pemanggilan di int main

--- nopewarisan.cpp
+++ nopewarisan.cpp
@@ -8,9 +8,16 @@ class baseClass final {
         }
 };
 
-class darivedClass : public baseClass {
+class derivedClass : public baseClass {
     public :
         void perkenalan() {
             cout << "Hallo saya Function dari derived class";
         }
 };
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0
+}
